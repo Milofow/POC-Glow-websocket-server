57dc90996f10f6bafe9d5f72b734398fc2502586
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2022-06-10 13:09:28 +0200

    Added POC

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors')
+app.use(cors())
+
+
+
+const server = http.createServer(app)
+
+const io = new Server(server, { 
+    cors: {
+        origin: "*",
+        methods: ["POST"]
+    }
+})
+
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`)
+    
+    socket.on("weather", (data) => {
+        console.log(data)
+        socket.broadcast.emit("callback", data)
+    })
+})
+
+
+server.listen(3001, () => {
+    console.log("Websocket server is running on port 3001")
+});
