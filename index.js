38417535c4e2d6652e79c329d645a50a8ec43f5a
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2022-06-11 22:37:08 +0200

    Push static files

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
+const INDEX = "index.html";
 
+app.use(cors())
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 
 const server = http.createServer(app)
 
@@ -34,4 +36,6 @@ server.listen(PORT, () => {
 
 
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
